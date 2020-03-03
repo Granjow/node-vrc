d3b30333a3abcfc5b3b76b5e075156f76d98b42e
Simon A. Eugster <simon.eu@gmail.com>
2020-03-03 14:02:37 +0100

    Help output: List options and improve readability

--- src/help.ts
+++ src/help.ts
@@ -4,16 +4,41 @@ import chalk from 'chalk';
 const wrapAnsi = require( 'wrap-ansi' );
 
 export const printHelp = ( appName : string, validArgs : VrcArgument[], invalidNames : Set<string>, conf : any, settings? : VrcSettings ) => {
+
     console.log( `Usage: ${process.argv[ 0 ]} [OPTIONS]\n` );
+
     if ( settings && settings.description ) {
         console.log( settings.description + '\n' );
     }
+
+    const colInvalid = chalk.bold.red;
+    const colValid = chalk.bold.blue;
+
+    const indent : string = '    ';
+    const lines : string[] = [];
+
     validArgs.forEach( ( el ) => {
-        const valueColor = invalidNames.has( el.name ) ? chalk.bold.red : chalk.bold.blue;
+        const valueColor = invalidNames.has( el.name ) ? colInvalid : colValid;
         const val = valueColor( conf[ el.name ] );
+
+        lines.push( `${chalk.green( el.name )} : ${el.type} (default: ${el.dflt}) → ${val}` );
+
+        if ( el.options ) {
+            lines.push( `${indent}${chalk.green( 'Options:' )} ${el.options.join( '  ' )}` );
+        }
+
         const desc = wrapAnsi( el.desc, 78 )
-            .replace( /^/gm, '    ' );
-        console.log( `${chalk.green( el.name )} : ${el.type} [${el.dflt}] → ${val}\n${desc}\n` );
+            .replace( /^/gm, indent );
+        lines.push( desc );
+
     } );
-    console.log( `Configuration files can be stored in .${appName}rc in this or a parent directory,\nor in another location checked by rc: https://www.npmjs.com/package/rc\n` );
+
+    lines.push( '' );
+    lines.push( `${colValid( 'Valid' )} and ${colInvalid( 'invalid' )} arguments are highlighted.` );
+
+    lines.push( '' );
+    lines.push( wrapAnsi( `Configuration files can be stored in .${appName}rc in this or a parent directory, ` +
+        'or in another location checked by rc: https://www.npmjs.com/package/rc', 78 ) );
+
+    console.log( lines.join( '\n' ) );
 };
